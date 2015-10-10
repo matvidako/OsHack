77828d3382eda530d59f18d7f185fc1ad87ed288
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2015-10-10 14:39:14 +0200

    added dummy heat map

--- app/src/main/java/hr/ms/oshack/MapsActivity.java
+++ app/src/main/java/hr/ms/oshack/MapsActivity.java
@@ -14,6 +14,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -83,9 +90,10 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
         }
 
         LatLng userPos = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-        CameraPosition center = new CameraPosition.Builder().zoom(15).target(userPos).build();
+        CameraPosition center = new CameraPosition.Builder().zoom(10).target(userPos).build();
         CameraUpdate update = CameraUpdateFactory.newCameraPosition(center);
         mMap.moveCamera(update);
+        addHeatMap();
     }
 
     @Override
@@ -95,4 +103,29 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
     }
+
+    private void addHeatMap() {
+        List<LatLng> list = new ArrayList<>();
+        Random random = new Random();
+
+        double latitude = mLastLocation.getLatitude();
+        double longitude = mLastLocation.getLongitude();
+        double limit = 10;
+
+        for(int i = 0; i < 50; i++) {
+            list.add(new LatLng(latitude+random.nextDouble(), longitude+random.nextDouble()));
+        }
+
+        for(int i = 0; i < 500; i++) {
+            list.add(new LatLng(latitude+random.nextDouble()/limit, longitude+random.nextDouble()/limit));
+        }
+
+        //Gradient gradient = new Gradient(new int[]{Color.rgb(0, 255, 0), Color.rgb(255, 0, 0)}, new float[]{0f, 1f});
+        HeatmapTileProvider mProvider = new HeatmapTileProvider.Builder()
+                .data(list)
+                //.gradient(gradient)
+                .build();
+        TileOverlay mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+
 }
